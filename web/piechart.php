a24ab9a9a5fc71233a890eea9dfbb0acc26ff8bb
Patrick Gartung <gartung@fnal.gov>
2021-06-29 20:12:17 +0200

    Slightly faster recursive glob

--- web/piechart.php
+++ web/piechart.php
@@ -46,14 +46,14 @@
 
       // from https://stackoverflow.com/a/17161106/2050986
       // does not support flag GLOB_BRACE
-      function rglob($pattern, $flags = 0) {
-          $files = glob($pattern, $flags);
-          foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR) as $dir) {
-              $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+      function rglob($basedir, $pattern, $flags = 0) {
+          $files = glob($basedir.'/'.$pattern, $flags);
+          foreach (glob($basedir.'/*', GLOB_ONLYDIR) as $dir) {
+              $files = array_merge($files, rglob($dir, $pattern, $flags));
           }
           return $files;
       }
-      print("var datasets = [ " . join(", ", array_map("preformat", rglob("data/*.json"))) . " ];\n");
+      print("var datasets = [ " . join(", ", array_map("preformat", rglob("data","*.json"))) . " ];\n");
       print("var groups = [ " . join(", ", array_map("preformat", glob("groups/*.json"))) . " ];\n");
       print("var colours = [ " . join(", ", array_map("preformat", glob("colours/*.json"))) . " ];\n");
     ?>
